bdb00c4da0a29ff281bab0cf3a215a44c8d6920d
T-e-R-a-D-e-v-i-l <teradevil@rambler.ru>
2023-02-22 13:51:21 +0300

    добавление в избранное командная работа

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-import { format, differenceInCalendarDays } from 'date-fns'
-import { ru } from 'date-fns/locale'
+import { format, differenceInCalendarDays } from "date-fns"
+import { ru } from "date-fns/locale"
+import swal from "sweetalert"
 
 async function makeTour() {
     const response = await fetch(
@@ -11,14 +12,22 @@ async function makeTour() {
 }
 
 function renderTours(tours) {
+    console.log(tours)
     const container2 = document.getElementById("container")
 
     container2.innerHTML = ""
+    if (tours.length === 0) {
+        container2.innerHTML = "к сожалению ничего не найдено"
+        return
+    }
 
     tours.forEach((tour) => {
-        const pattern = 'dd MMMM y'
-        const option = {locale: ru}
-        const duration = differenceInCalendarDays(new Date(tour.endTime), new Date(tour.startTime))
+        const pattern = "dd MMMM y"
+        const option = { locale: ru }
+        const duration = differenceInCalendarDays(
+            new Date(tour.endTime),
+            new Date(tour.startTime)
+        )
 
         container2.innerHTML += `
         
@@ -28,12 +37,24 @@ function renderTours(tours) {
 
                     <div class="p-6">
                         <div>
+                            <div class="flex justify-end">
+                            <button id="addFavorit${
+                                tour.id
+                            }" class="btn-primary mt-4">Добавить в избранное</button>
+                            <button id="deleteFavorit${
+                                tour.id
+                            }" class="btn-primary mt-4 hidden">Удалить из избранного</button>
+                            </div>
                             <p class="text-yellow-600 font-medium text-sm hover:underline">
                                 <a href="#">${tour.country}</a>
                             </p>
                             <a href="#">
-                                <p class="font-semibold mt-3 text-xl">${tour.city}</p>
-                                <p class="text-gray-500 mt-3">${tour.hotelName}</p>
+                                <p class="font-semibold mt-3 text-xl">${
+                                    tour.city
+                                }</p>
+                                <p class="text-gray-500 mt-3">${
+                                    tour.hotelName
+                                }</p>
                             </a>
                         </div>
 
@@ -51,7 +72,15 @@ function renderTours(tours) {
                         <div class="mt-3 text-gray-500 text-sm flex items-center">
 
                             <p class="ml-1">
-                            ${format(new Date(tour.startTime), pattern, option)} - ${format(new Date(tour.endTime), pattern, option)}
+                            ${format(
+                                new Date(tour.startTime),
+                                pattern,
+                                option
+                            )} - ${format(
+            new Date(tour.endTime),
+            pattern,
+            option
+        )}
                             </p>
                             
                         </div>
@@ -61,23 +90,90 @@ function renderTours(tours) {
                         </div>
 
                         <button class="btn-primary mt-4">Подробнее</button>
-
+                        
                     </div>
             </div>
         `
+
+        if (favorites.includes(tour.id)) {
+            deleteBtn(tour)
+        } else {
+            addBtn(tour)
+        }
+    })
+
+    document.getElementById(`favoritBtn`).addEventListener("click", () => {
+        const favTours = tours.filter((t) => {
+            return favorites.includes(t.id)
+        })
+        renderTours(favTours)
+    })
+
+    tours.forEach((tour) => {
+        if (favorites.includes(tour.id)) {
+            document
+                .getElementById(`deleteFavorit${tour.id}`)
+                .addEventListener("click", () => {
+                    swal("Тур удален из избранного")
+                    addBtn(tour)
+                    deleteFavorites(tour.id)
+                    saveToLocalStorage()
+                })
+        } else {
+            document
+                .getElementById(`addFavorit${tour.id}`)
+                .addEventListener("click", () => {
+                    favorites.push(tour.id)
+                    swal("Тур добавлен в избранное")
+                    deleteBtn(tour)
+                    saveToLocalStorage()
+                })
+        }
     })
 }
 
-const tourCity = document.getElementById('tour.city');
-const tourCountry = document.getElementById('tour.country');
+function addBtn(tour) {
+    document.getElementById(`addFavorit${tour.id}`).style.display = "flex"
+    document.getElementById(`deleteFavorit${tour.id}`).style.display = "none"
+}
+
+function deleteBtn(tour) {
+    document.getElementById(`addFavorit${tour.id}`).style.display = "none"
+    document.getElementById(`deleteFavorit${tour.id}`).style.display = "flex"
+}
+
+// добавление в избранное
+
+let favorites = [] // массив id избранных туров
+
+//сохранение данных из localStorage
+function saveToLocalStorage() {
+    const toursJson = JSON.stringify(favorites)
+    localStorage.setItem("favorites", toursJson)
+}
+
+//получение данных из localStorage
+const toursJson = localStorage.getItem("favorites")
+if (toursJson) {
+    favorites = JSON.parse(toursJson)
+}
+
+function deleteFavorites(tour) {
+    const index = favorites.indexOf(tour.id)
+    favorites.splice(index, 1)
+}
+
+// блок с фильтрацией туров
+const tourCity = document.getElementById("tour.city")
+const tourCountry = document.getElementById("tour.country")
 
 function getCity(tourCity) {
-    let checkCity;
+    let checkCity
 
     if (tourCity == null) {
-        checkCity = tourCountry;
+        checkCity = tourCountry
     } else {
-        checkCity = tourCity;
+        checkCity = tourCity
     }
     return checkCity
 }
@@ -109,6 +205,10 @@ async function init() {
     document
         .getElementById("all")
         .addEventListener("click", () => filterByCountry(tours))
+
+    document.getElementById(`allToursBtn`).addEventListener("click", () => {
+        renderTours(tours)
+    })
 }
 
 init()
